8b06bc07bb2ad7390fab2ee013a3e36ef5927f7f
Kristian Sølvberg <kristian@wtw.no>
2022-02-17 12:56:23 +0100

    Version 2.03

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button.Button;
+  }
+});
+Object.defineProperty(exports, "ColorPicker", {
+  enumerable: true,
+  get: function get() {
+    return _ColorPicker.ColorPicker;
+  }
+});
+
+var _Button = require("./components/Button/Button");
+
+var _ColorPicker = require("./components/ColorPicker/ColorPicker");
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-import {Button} from "./Button/Button";
-import {ColorPicker} from "./ColorPicker/ColorPicker";
-
-export {Button, ColorPicker}

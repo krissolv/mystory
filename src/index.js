ace3a979b193913a8891c56d1dec2a60a9218b49
Kristian Sølvberg <kristian@wtw.no>
2022-02-17 12:40:07 +0100

    Wow. Such new folder structure.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {Button} from "./components/Button/Button";
+import {ColorPicker} from "./components/ColorPicker/ColorPicker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export {Button, ColorPicker}

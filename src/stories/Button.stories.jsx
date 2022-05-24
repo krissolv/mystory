c8b5ba9d7a704bc8e2650f2abf2bf76d26cf761e
Kristian Sølvberg <kristian@wtw.no>
2022-05-24 13:40:41 +0200

    So much stuff

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -23,14 +23,14 @@ Secondary.args = {
   label: 'Button',
 };
 
+export const Small = Template.bind({});
+Small.args = {
+	size: 'small',
+	label: 'Button',
+};
+
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
   label: 'Button',
 };
